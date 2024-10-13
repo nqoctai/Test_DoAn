4d012baee1036918849971699fa2772d0c6558e2
nqoctai <2001224231@hufi.edu.vn>
2024-10-13 21:13:02 +0700

    TaiChange

---
+++ Test_DoAn/Controllers/TaiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_DoAn.Controllers
+{
+    public class TaiController : Controller
+    {
+        // GET: Tai
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
